a52fc8d56d942e24e5ad5d4b75a12ea09c50dfc1
Ali <alistairaldo@hotmail.co.uk>
2024-10-27 17:17:40 +0000

    Turned functions into screen classes

--- src/Game/Game.py
+++ src/Game/Game.py
@@ -1,6 +1,7 @@
 import pygame, sys
-from Sprites import Enemy
-from Screens import StartMenu, GameOver
+from Screens.StartMenu import StartMenu
+from Screens.Fight import Fight
+from Screens.GameOver import GameOver
 from pygame.locals import *
 pygame.init()
 
@@ -17,7 +18,7 @@ class Game:
         self.fpsClock = pygame.time.Clock()
         self.time = 0.00
         self.fps = fps
-        self.screens = [StartMenu(self), GameOver(self)]
+        self.screens = [StartMenu(self), Fight(self), GameOver(self)]
         #Load main Music
         pygame.mixer.music.load("assets/music.ogg")
         pygame.mixer.music.play(-1)
@@ -36,77 +37,6 @@ class Game:
                     pygame.display.flip()
                     #FPS
                     self.time = float(self.fpsClock.Clock.tick_busy_loop(self.fps) / 1000.00)
-            # #Displays start menu
-            # self.startMenu()
-            # #Runs game, stores returned score when completed
-            # score = self.game()
-            # #When the game is over, if user decides to play again true is returned and score is returned as 0
-            # self.gameOver(score)
-            # #If game hasn't ended, Resets Enemy class list
-            # Enemy.reset()
-
-    def runScreen(self, screen):
-        while screen.running:
-            #Checks for events and stores them in local variables
-            self.handleEvents()
-            self.handleKeyPresses()
-            #Blit to Screen
-            screen.run(self)
-            #FPS
-            self.time = float(self.fpsClock.Clock.tick_busy_loop(self.fps) / 1000.00)
-
-
-
-    def game():
-        #bool to turn while loop on and off
-        gameRunning = True
-        #Sets up background
-        gameBackground = 
-        #Creates player object
-        gamePlayer = Player()
-        #Variable to keep track of score
-        gameScore = 0
-        #Creates clock object to limit FPS, time to hold calls to fpsClock.tick_busy_loop() to track game time
-        gameFPSClock = pygame.time.Clock()
-        gameTime = 0.00
-
-        while gameRunning:
-            #Draws background to screen
-            screen.blit(gameBackground, [0,0])
-
-            #Detects user input
-            gameEvents = pygame.event.get()
-            gameKey = pygame.key.get_pressed()
-
-            #Draws Player and Detects Player Movement Key Press
-            gamePlayer.move(gameKey)
-            gamePlayer.draw(screen)
-
-            #Generates Enemys and Tests for Player Collision
-            Enemy.generate()
-            Enemy.draw(screen)
-            Enemy.move()
-            gameRunning = Enemy.collide(gamePlayer.rect)
-
-            #Generates Projectiles and Tests for Enemy hit detection
-            Projectile.generate(gameKey, gamePlayer)
-            Projectile.draw(screen)
-            Projectile.move()
-            gameScore += Projectile.collide()
-
-            #Checks events
-            Quit(gameEvents)
-            Fullscreen(gameKey)
-
-            #Updates display
-            pygame.display.flip()
-
-            #FPS setting
-            gameTime = float(gameFPSClock.tick_busy_loop(60) / 1000.00)
-            Enemy.time += gameTime
-            Projectile.time += gameTime
-
-        return gameScore
 
     #Used to exit the game if x in pygame window is pressed
     def handleEvents(self):
--- src/Screens/Fight.py
+++ src/Screens/Fight.py
@@ -1,6 +1,23 @@
 from Screen import Screen
+from Sprites.Player import Player
+from Sprites.Enemy import Enemy
+from Sprites.Projectile import Projectile
 import pygame
 
 class Fight(Screen):
     def __init__(self, game) -> None:
-        super().__init__(pygame.image.load("assets/background.jpg").convert(), sprites, ui, game)
+        self.player = Player()
+        super().__init__(pygame.image.load("assets/background.jpg").convert(), [self.player], [], game)
+
+    def draw(self):
+        super().draw()
+        Enemy.draw(self.game.surface)
+        Projectile.draw(self.game.surface)
+
+    def handleSprites(self):
+        self.running = Enemy.collide(self.player.rect)
+        self.game.score += Projectile.collide()
+        Enemy.generate()
+        Projectile.generate(self.player)
+        Enemy.time += self.game.time
+        Projectile.time += self.game.time
